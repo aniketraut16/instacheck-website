1924734131ead5fc0860e9af0575e6cd1937ddfd
aniketraut16 <rautnaniket@gmail.com>
2025-09-16 18:28:08 +0530

    Update dependencies, enhance TypeScript configuration, and implement LiquidEther component in App. Added custom CSS variables and animations for improved styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import LiquidEther  from './components/LiquidEther'; 
 function App() {
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className='w-full h-screen relative bg-black'>      
+      <LiquidEther
+        colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+        mouseForce={20}
+        cursorSize={100}
+        isViscous={false}
+        viscous={30}
+        iterationsViscous={32}
+        iterationsPoisson={32}
+        resolution={0.5}
+        isBounce={false}
+        autoDemo={true}
+        autoSpeed={0.5}
+        autoIntensity={2.2}
+        takeoverDuration={0.25}
+        autoResumeDelay={3000}
+        autoRampDuration={0.6}
+      />
+    </div>
   )
 }
 
 export default App
+
+
